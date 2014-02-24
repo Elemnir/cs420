bb6429c2ef55222d0da3d7432a1d7c303705a68d
Elemnir <ahoward0920@gmail.com>
2014-02-24 10:44:53 -0500

    Fixed the bug, works for single run tests

--- hopfield/main.cpp
+++ hopfield/main.cpp
@@ -14,46 +14,50 @@ map<int, double> avgNumberStable; //final output for graph 2
 
 void imprintPatterns(int p)
 {
-	double sum = 0.0f;
+	double sum;
 	for (int i = 0; i < neurons.size(); ++i)
+	{
 		for (int j = 0; j < neurons.size(); ++j)
 		{
+			sum = 0.0f;
 			if (i != j)	
-				for (int k = 0; k < p && k < patterns.size(); ++k)
+			{
+				for (int k = 0; k < p; ++k)
 					sum += patterns[k][i] * patterns[k][j];
+			}
 			weights[i][j] = sum / neurons.size();
 		}
+	}
 }
 
 void testPatterns(int p)
 {
 	double sum;
 	int unstableCount = 0, sprime;
-	for (int k = 0; k < p && k < patterns.size(); ++k)
+	for (int k = 0; k < p; ++k)
 	{
 		neurons = patterns[k];
 		for (int i = 0; i < neurons.size(); ++i)
 		{
 			sum = 0.0f;
-			for (int j = 0; j < patterns[k].size(); ++j)
+			for (int j = 0; j < neurons.size(); ++j)
 				sum += weights[i][j] * neurons[j];
 
 			sprime = (sum < 0.0f) ? -1.0f : 1.0f;
-			if (neurons[i] != sprime);
+			if (neurons[i] != sprime)
 			{
 				unstableCount++;
 				break;
 			}
 		}
 	}
-
+	cout << "Unstable: " << unstableCount << endl;
 	avgPercentUnstable[p] += (double(unstableCount) / p);
 	avgNumberStable[p] += p - unstableCount;
 }
 
 int main(int argc, char** argv)
 {
-	//C++ is wizardry. All shall bow before its mighty random number generation
 	mt19937 rng(random_device{}()); //constructs the random number generator
 	discrete_distribution<int> dist {1, 0, 1}; //generates 0's and 2's
 	weights.resize(100, vector<double>(100, 0.0f));
@@ -62,13 +66,22 @@ int main(int argc, char** argv)
 	for (int i = 0; i < numruns; i++)
 	{
 		//initialize the pattern set
-		patterns = vector<vector<int> >(50, vector<int>(100, dist(rng) - 1));
+		//patterns = vector<vector<int> >(50, vector<int>(100, dist(rng) - 1));
+		patterns.clear();
+		for (int x = 0; x < 50; ++x)
+		{
+			patterns.emplace_back(100);
+			for (int y = 0; y < patterns.back().size(); ++y)
+				patterns.back().at(y) = dist(rng) - 1;
+		}
+		
 		for (int p = 1; p <= patterns.size(); ++p)
 		{
 			imprintPatterns(p);
 			testPatterns(p);
 		}
 	}
+
 
 	for (int i = 1; i <= 50; ++i)
 	{
